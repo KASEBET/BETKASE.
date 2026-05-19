52b398d6f8fe331314d27b983d342340035edcc8
KASEBET <camargosantiago2003@gmail.com>
2026-05-19 10:59:39 -0500

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,23 @@
+export const SOCCER_LEAGUES = [
+  { name: 'Premier League', slug: 'eng.1', country: 'Inglaterra' },
+  { name: 'La Liga', slug: 'esp.1', country: 'España' },
+  { name: 'Bundesliga', slug: 'ger.1', country: 'Alemania' },
+  { name: 'Serie A', slug: 'ita.1', country: 'Italia' },
+  { name: 'Ligue 1', slug: 'fra.1', country: 'Francia' },
+  { name: 'Champions League', slug: 'uefa.champions', country: 'Europa' },
+  { name: 'Europa League', slug: 'uefa.europa', country: 'Europa' },
+  { name: 'Copa Libertadores', slug: 'conmebol.libertadores', country: 'Sudamérica' },
+  { name: 'Liga BetPlay', slug: 'col.1', country: 'Colombia' },
+  { name: 'Brasileirao', slug: 'bra.1', country: 'Brasil' },
+  { name: 'Liga MX', slug: 'mex.1', country: 'México' },
+  { name: 'Super Lig', slug: 'tur.1', country: 'Turquía' }
+];
+
+export const NBA_LEAGUES = [
+  { name: 'NBA', slug: 'nba', sport: 'basketball' }
+];
+
+export const TENNIS_TOURS = [
+  { name: 'ATP Tour', slug: 'atp', sport: 'tennis' },
+  { name: 'WTA Tour', slug: 'wta', sport: 'tennis' }
+];
